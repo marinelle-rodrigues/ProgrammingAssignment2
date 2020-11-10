ccecc4c68fad58c3773e140d211a93d6cde25b04
marinelle-rodrigues <73085467+marinelle-rodrigues@users.noreply.github.com>
2020-11-09 23:43:09 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function takes matrix and returns inverse and caches it
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  matChanged<-TRUE
+  #set the matrix first
+  setMatrix <- function(x) {
+    m <<- x
+    inv <<- NULL
+    matChanged <<- TRUE
+  }
+  #get the current matrix
+  getMatrix <- function() m
+  #set up the inverse matrix
+  setMatInverse <- function(inverse) {
+    matChanged <<- FALSE
+    inv <<- inverse
+  }
+  
+  getMatInverse <- function() inv
+  
+  isMatChanged <- function() matChanged
+  
+  list(setMatrix = setMatrix, 
+       getMatrix = getMatrix,
+       setMatInverse = setMatInverse,
+       getMatInverse = getMatInverse,
+       isMatChanged = isMatChanged)
 }
-
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheMatrix <- function(mmatrix, ...) {
+  inv <- mmatrix$getMatInverse()
+  isMChanged <- mmatrix$isMatChanged()
+  if(!is.null(inv) && !isMChanged) {
+    message("Getting cached inverse of matrix")
+    return(inv)
+  }
+  m <- mmatrix$getMatrix()
+  inv <- solve(m)
+  mmatrix$setMatInverse(inv)
+  
+  message("calculating matrix inverse")
+  inv
 }
+        ## Return a matrix that is the inverse of 'x'
+
